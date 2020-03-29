fc633a7e2922fb061873848371ef7f91acd8abf9
charu11 <charunumesh@gmail.com>
2020-03-29 22:48:33 +0530

    second commit

--- pydjango/new_app/views.py
+++ pydjango/new_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tutorials
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
+
 
 def home(request):
     return render(request=request,
@@ -9,3 +12,20 @@ def home(request):
                   context={'tutorials': Tutorials.objects.all}
 
                   )
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('new_app:homepage')
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+    form = UserCreationForm
+    return render(request,
+                  "new_app/register.html",
+                  context={'form': form})
